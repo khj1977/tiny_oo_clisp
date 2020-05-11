18071f4c9ddea6f784f20f993e29671b7d5de7b1
Hwi Jun KIM <euler.bonjour@gmail.com>
2020-05-11 10:20:40 +0000

    added myLoad function to load from current dir with no "./". Which could be extended to use project root not current dir.

---
+++ src/init.lisp
@@ -0,0 +1,14 @@
+(defun getCurrentDir ()
+  (truename "./")
+)
+
+(defun myLoad (relativePath)
+  (setf currentdir (getCurrentDir))
+  (setf path (merge-pathnames currentdir relativepath))
+  (if (eq (probe-file path) nil)
+    (progn
+      (return-from nil)
+    )
+  )
+  (load path)
+)
